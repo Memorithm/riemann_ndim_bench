448017ce9405874da14bcdaccc53a283ad68a7f3
MEMOPERF <contact@checkupauto.fr>
2026-09-07 09:58:40 +0200

    feat: export sine pairing refinement grid

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,7 @@ pub mod weil_quadratic_matrix;
 pub mod weil_refinement;
 pub mod weil_sine_direct_q;
 pub mod weil_sine_pairing;
+pub mod weil_sine_pairing_refinement;
 pub mod weil_sine_truncation;
 pub mod weil_subspace;
 pub mod weil_support;
